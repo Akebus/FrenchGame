6bf55e41aa719c95b9457732798927c6d2c83f22
Akebus <ake.engman@gmail.com>
2021-05-04 20:53:41 +0200

    Initial commit

---
+++ FrenchGame/Data/GameService.cs
@@ -0,0 +1,25 @@
+﻿using FrenchGame.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Text.Json;
+
+namespace FrenchGame.Data
+{
+    public class GameService
+    {
+
+        public List<GameItemModel> GetGameItems()
+        {
+            // Task<UserModel[]>
+
+            string json = System.IO.File.ReadAllText("Data/GameItems.json");
+            List<GameItemModel> gameItems = Newtonsoft.Json.JsonConvert.DeserializeObject<List<GameItemModel>>(json);
+
+
+            return gameItems;
+        }
+
+    }
+}   
---
+++ FrenchGame/Models/GameItemModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FrenchGame.Models
+{
+    public class GameItemModel
+    {
+        public string English { get; set; }  
+        public string French { get; set; }
+
+
+      
+
+
+
+
+    }
+}
